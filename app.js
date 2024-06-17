bb948ee1f79bf04e8f26ad699b82484df930cae4
Faelx11 <rafael.silva744@gmail.com>
2024-06-17 02:28:24 -0300

    Armazem de Imagens - NodeJS

---
+++ app.js
@@ -0,0 +1,73 @@
+// Imports
+const { 
+    app,
+    port,
+    upload,
+    bucket 
+} = require('./config/config.js')
+
+// Initialize Server
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`)
+})
+
+// Main Route
+app.get('/', (req, res) => {
+    res.render('subirImagens')
+})
+
+    // Post Route
+    app.post('/upload', upload.single('file'), async (req, res) => {
+        try {
+            console.log('Arquivo recebido:', req.file); // Adicione este log
+        
+            if (!req.file) {
+            res.status(400).send('Nenhum arquivo foi enviado.');
+            return;
+            }
+        
+            const blob = bucket.file(req.file.originalname);
+            const blobStream = blob.createWriteStream({
+            metadata: {
+                contentType: req.file.mimetype
+            }
+            });
+        
+            blobStream.on('error', (err) => {
+            console.error(err);
+            res.status(500).send('Erro ao fazer upload do arquivo.');
+            });
+        
+            blobStream.on('finish', () => {
+            res.redirect('/galeria');
+            });
+        
+            blobStream.end(req.file.buffer);
+        } catch (error) {
+            console.error(error);
+            res.status(500).send('Erro ao fazer upload do arquivo.');
+        }
+    });
+      
+    // Gallery Route
+    app.get('/galeria', async (req, res) => {
+        try {
+            const [files] = await bucket.getFiles();
+            
+            // Mapeia os arquivos para URLs das imagens e nomes dos arquivos
+            const images = files.map(file => {
+                return {
+                    url: `https://storage.googleapis.com/${bucket.name}/${file.name}`,
+                    name: file.name
+                };
+            });
+    
+            console.log(images); // Verifique as URLs e nomes das imagens no console
+    
+            res.render('galeria', { images });
+        } catch (error) {
+            console.error(error);
+            res.status(500).send('Erro ao obter as imagens.');
+        }
+    });
+    
---
+++ config/config.js
@@ -0,0 +1,60 @@
+// Imports
+const { 
+    express, 
+    handlebars, 
+    bodyParser, 
+    initializeApp, 
+    getFirestore,
+    multer,
+    path,
+    admin,
+    serviceAccount
+} = require('./imports')
+
+// Config Express
+const app = express()
+const port = 8081
+
+
+// Configuração da View Engine
+app.use(express.static('public'));
+    
+app.engine('handlebars', handlebars({
+    defaultLayout: 'main',
+
+}))
+
+// Utilização da View Engine
+    app.set("view engine", "handlebars")
+
+// Pasta padrao de views
+    app.set('views', path.join(__dirname, '../views'));
+
+// Pasta padrao de css
+    app.use(express.static(path.join(__dirname, '../public')));
+
+
+// Config Body Parser
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// Config Firebase
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket: 'gs://photostorage-23f94.appspot.com'
+})
+
+// Config Bucket
+const bucket = admin.storage().bucket()
+
+// Config Multer
+const upload = multer({
+    storage: multer.memoryStorage()
+});
+
+module.exports = {
+    app,
+    port,
+    upload,
+    bucket
+}
